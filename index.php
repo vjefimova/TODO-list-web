d32c1f3a413a79ed9b7973c2e5fbdbce879f7ddd
Veronika <veronika.jefimova03@gmail.com>
2021-11-29 16:12:12 +0200

    Create login form

--- index.php
+++ index.php
@@ -33,12 +33,11 @@ if(isset($_REQUEST["edit"])) {
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css">
-    <script src="js/app.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>TODO-list</title>
 </head>
 <body>
-<section class="vh-100 gradient-custom-2">
+<section class="vh-100">
     <div class="container  py-5 h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
             <div class="col-md-15 col-xl-14">
